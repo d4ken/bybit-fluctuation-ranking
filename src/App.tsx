fee5d237b8b03c901a997ceb95c5599b55bbd92d
Daiki Shikencho <112839844+d4ken@users.noreply.github.com>
2024-03-03 20:12:23 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {TickerData} from "types/Ticker";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    let tickerArray: TickerData[] = [{
+        symbol: "",
+        lastPrice: 0
+    }, {
+        symbol: "",
+        lastPrice: 0
+    }];
+
+    const [ticker, setTicker] = useState<TickerData[]>(tickerArray);
+    let symbols = ["BTCUSDT", "ETHUSDT"]
+
+    useEffect(() => {
+        const endpoint = "wss://stream.bybit.com/v5/public/spot"
+        const ws = new WebSocket(endpoint);
+        const apiCall = {"op": "subscribe", "args": ["tickers.BTCUSDT", "tickers.ETHUSDT"]};
+        ws.onopen = () => {
+            console.log('"open" event!');
+            console.log('WebSocket Client Connected');
+            setInterval(() => {
+            }, 3000);
+            ws.send(JSON.stringify(apiCall));
+        };
+
+        //  購読した値を取得
+        ws.onmessage = (event) => {
+            const json = JSON.parse(event.data);
+            try {
+                if (json.data !== undefined) {
+                    if (json.data.symbol === symbols[0]) {
+                        let lastPrice = Number(json.data.lastPrice)
+                        const tmp = [...tickerArray];
+                        tmp[0].symbol = json.data.symbol;
+                        tmp[0].lastPrice = lastPrice;
+                        setTicker(() => tmp);
+                    }
+                    if (json.data.symbol === symbols[1]) {
+                        let lastPrice = Number(json.data.lastPrice)
+                        const tmp = [...tickerArray];
+                        tmp[1].symbol = json.data.symbol;
+                        tmp[1].lastPrice = lastPrice;
+                        setTicker(() => tmp);
+                    }
+                }
+            } catch (err) {
+                console.log(err);
+            }
+        };
+    }, [])
+
+    return (
+        <main>
+            <div>
+                <ul>
+                    <li>
+                        {ticker[0].symbol}
+                    </li>
+                    <li>
+                        last : {ticker[0].lastPrice}
+                    </li>
+
+                    <li>
+                        {ticker[1].symbol}
+                    </li>
+                    <li>
+                        last : {ticker[1].lastPrice}
+                    </li>
+                </ul>
+            </div>
+        </main>
+    );
 }
 
 export default App;
