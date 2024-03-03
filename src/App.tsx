01e49d68a84637fc3f39d326cb8eaa51e3322821
Daiki Shikencho <112839844+d4ken@users.noreply.github.com>
2024-03-03 23:48:38 +0900

    配列数指定で表示コイン数を変更できるように修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,49 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
 import {TickerData} from "types/Ticker";
+import RankingData from "./listing.json";
+import CryptoInfo from "./components/CryptoInfo";
+export const CMC_CRYPTO_AMOUNT = 10;
 
 const App = () => {
-    let tickerArray: TickerData[] = [{
-        symbol: "",
-        lastPrice: 0
-    }, {
-        symbol: "",
-        lastPrice: 0
-    }];
-
+    let tickerArray = new Array<TickerData>(CMC_CRYPTO_AMOUNT);
+    for (let i = 0; i < CMC_CRYPTO_AMOUNT; i++) {
+        tickerArray[i] = {
+            symbol: "",
+            lastPrice: 0
+        };
+    }
     const [ticker, setTicker] = useState<TickerData[]>(tickerArray);
-    let symbols = ["BTCUSDT", "ETHUSDT"]
-
     useEffect(() => {
+        // cmc crypto listing json data reader
+        let coinSymbolList: string[] = [];
+
+        for (const key in RankingData) {
+            if (key === "data") {
+                for (const val of RankingData[key]) {
+                    if (val.symbol !== "USDT")
+                        coinSymbolList.push(val.symbol)
+                }
+            }
+        }
+
+        let coinPairs = coinSymbolList
+            .filter((val, index) => {
+                if (index < CMC_CRYPTO_AMOUNT) return val;
+            })
+            .map((val) => {
+                return `${val}USDT`
+            })
+
+        let coinTickerNames = coinPairs
+            .map((val) => {
+                return `tickers.${val}`
+            })
+
         const endpoint = "wss://stream.bybit.com/v5/public/spot"
         const ws = new WebSocket(endpoint);
-        const apiCall = {"op": "subscribe", "args": ["tickers.BTCUSDT", "tickers.ETHUSDT"]};
+        const apiCall = {"op": "subscribe", "args": coinTickerNames};
+
         ws.onopen = () => {
             console.log('"open" event!');
             console.log('WebSocket Client Connected');
@@ -31,20 +57,15 @@ const App = () => {
             const json = JSON.parse(event.data);
             try {
                 if (json.data !== undefined) {
-                    if (json.data.symbol === symbols[0]) {
-                        let lastPrice = Number(json.data.lastPrice)
-                        const tmp = [...tickerArray];
-                        tmp[0].symbol = json.data.symbol;
-                        tmp[0].lastPrice = lastPrice;
-                        setTicker(() => tmp);
-                    }
-                    if (json.data.symbol === symbols[1]) {
-                        let lastPrice = Number(json.data.lastPrice)
-                        const tmp = [...tickerArray];
-                        tmp[1].symbol = json.data.symbol;
-                        tmp[1].lastPrice = lastPrice;
-                        setTicker(() => tmp);
-                    }
+                    coinPairs.forEach((val, index) => {
+                        if (json.data.symbol === val) {
+                            let lastPrice = Number(json.data.lastPrice)
+                            const tmp = [...tickerArray];
+                            tmp[index].symbol = json.data.symbol;
+                            tmp[index].lastPrice = lastPrice;
+                            setTicker(() => tmp);
+                        }
+                    })
                 }
             } catch (err) {
                 console.log(err);
@@ -52,23 +73,20 @@ const App = () => {
         };
     }, [])
 
+    const showTickers = () => {
+        return ticker.map((item, index) => {
+            return <div>
+                <h2>{index + 1}</h2>
+                <CryptoInfo symbol={item.symbol} lastPrice={item.lastPrice}/>
+            </div>
+        })
+    }
+
     return (
         <main>
             <div>
                 <ul>
-                    <li>
-                        {ticker[0].symbol}
-                    </li>
-                    <li>
-                        last : {ticker[0].lastPrice}
-                    </li>
-
-                    <li>
-                        {ticker[1].symbol}
-                    </li>
-                    <li>
-                        last : {ticker[1].lastPrice}
-                    </li>
+                    {showTickers()}
                 </ul>
             </div>
         </main>
